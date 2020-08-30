5b35af7cb019ad3dc0796782c9cae3430c785018
yuki678 <uk.12juni@gmail.com>
2020-08-30 08:22:56 +0100

    updated directories

--- setup_env.sh
+++ setup_env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-WS_PATH=/home/workspace/object_detection
+export WS_PATH=/home/workspace/object_detection
 
 conda create -n od-py37 pip python=3.7
 conda init bash
@@ -9,14 +9,15 @@ conda activate od-py37
 pip install --upgrade tensorflow-gpu==1.14
 
 conda install pillow lxml jupyter matplotlib opencv cython pandas
-cd $WS_PATH/TensorFlow/models/research
+cd $WS_PATH/Tensorflow/models/research
 pip install .
 pip install labelImg
 
 # Some modules required avoid errors
 pip install absl-py gast==0.2.2 pycocotools
 
-mkdir /home/backups/training && cd "$_"
-cp $WS_PATH/workspace/config/faster_rcnn_resnet101_kitti.config .
+# Create training directory - this is deleted everytime you reconnect
+export LOG_PATH=/home/backups/training
+mkdir $LOG_PATH
 
 cd $WS_PATH/workspace
